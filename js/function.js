1b2f5b9b1a34f3a034c200bc3c7562a218d86ee7
Semyon Turchaninov <special.for.origi@gmail.com>
2024-07-05 22:40:56 +0300

    module2-task1

--- js/function.js
+++ js/function.js
@@ -7,21 +7,17 @@ const stringPalindromCheck = (string) => {
   const stringDespase = string.replaseAll(' ','');
   const stringLowerCase = stringDespase.toLowerCase();
   let stringReverse = '';
-  for (let letterNumber = stringLowerCase.length - 1; letterNumber >= 0; letterNumber --) {
-    stringReverse += stringLowerCase.at(letterNumber);
+  for (let i = stringLowerCase.length - 1; i >= 0; i --) {
+    stringReverse += stringLowerCase.at(i);
   }
   return string === stringReverse;
 };
 stringPalindromCheck('sus');
 
 //Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
-const figuresInString = (string) => {
-  let allFigures = '';
-  for (let figures = 0; figures <= string.length; figures ++) {
-    if(!Number.isNaN(parseInt(string.at(figures), 10))) {
-      allFigures += parseInt(string.at(figures), 10);
-    }
-  }
-  return allFigures;
-};
-figuresInString('lmao.l.0.l');
+const figuresOnly = (string) => {
+  const allFigures = string.replace(/[^0-9]/g, '');
+  return parseInt(allFigures, 10);
+}
+figuresOnly('123.пришлось_вкатиться_в_экспрешон.456');
+
