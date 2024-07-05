c95a38e72afb7968d0ed068a7a18b33f2be02813
Semyon Turchaninov <special.for.origi@gmail.com>
2024-07-05 20:14:04 +0300

    module2-task1

---
+++ js/function.js
@@ -0,0 +1,25 @@
+//Функция для проверки длины строки
+const stringLenghtCheck = (string,symbols) => string.length <= symbols;
+
+//Функция для проверки, является ли строка палиндромом
+const stringPalindromCheck = (string) => {
+  const stringDespase = string.replaseAll(' ','');
+  const stringLowerCase = stringDespase.toLowerCase();
+  let stringReverse = '';
+  for (let letterNumber = stringLowerCase.length - 1; letterNumber >= 0; letterNumber --) {
+    stringReverse += stringLowerCase.at(letterNumber);
+  }
+  return string === stringReverse;
+};
+
+//Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
+const figuresInString = (string) => {
+  let allFigures = '';
+  let figures;
+  for (let figures = 0; figures <= string.length; figures ++) {
+    if(!Number.isNaN(parseInt(string.at(figures), 10))) {
+      allFigures += parseInt(string.at(figures), 10);
+    }
+  }
+  return allFigures;
+};
